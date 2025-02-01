5fff7bcb4dbec68c3eb0c079d0c4a61a8e4bbfac
kanadeiar <kanadeiar@gmail.com>
2025-02-01 17:51:37 +0300

    Исправление ошибок

--- Lesson1/Task1TransactionScript/src/Task1.QuestionnaireSub.MainLogicLayer/QuestionnaireModule/Formatting/GluedLineCode.cs
+++ Lesson1/Task1TransactionScript/src/Task1.QuestionnaireSub.MainLogicLayer/QuestionnaireModule/Formatting/GluedLineCode.cs
@@ -3,5 +3,5 @@
 public record GluedLineCode : FormatCode
 {
     public override string FormatText((int id, string surName, string name, int age, int height, int weight) values) =>
-        values.Item1 + " " + values.Item2 + " " + values.Item3 + " лет " + values.Item4 + " см " + values.Item5 + " кг";
+        values.surName + " " + values.name + " " + values.age + " лет " + values.height + " см " + values.weight + " кг";
 }
